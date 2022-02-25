bd499eb24c3e6175e781a3c902727bf1f475b4eb
Yihua Zhang <yihuazhang2@gmail.com>
2022-02-25 15:31:50 -0500

    fixing name of productsContext

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,12 +2,12 @@ import { useContext } from 'react';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
-import { ProductContext } from '../../contexts/products.context';
+import { ProductsContext } from '../../contexts/products.context';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { products } = useContext(ProductContext);
+  const { products } = useContext(ProductsContext);
 
   return (
     <div className='products-container'>
