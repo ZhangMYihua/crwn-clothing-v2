a2217ad04012c2c52b78f73eb86b4129df13fb4d
Yihua Zhang <yihuazhang2@gmail.com>
2022-02-24 23:09:03 -0500

    updating function name

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -6,7 +6,6 @@ import Button from '../button/button.component';
 import {
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 import './sign-in-form.styles.scss';
