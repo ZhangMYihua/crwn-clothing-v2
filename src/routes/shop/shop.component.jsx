a34fce76ec4ac7beae45c1cbc0d266551bb39271
Yihua Zhang <yihuazhang2@gmail.com>
2022-02-27 11:52:15 -0500

    created category-preview component and added to shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import { useContext, Fragment } from 'react';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
@@ -11,16 +12,10 @@ const Shop = () => {
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
     </Fragment>
   );
 };
