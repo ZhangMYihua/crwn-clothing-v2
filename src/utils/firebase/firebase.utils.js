a8f5c4d33dda4010c2cbb34ab4057f18ba3137c0
Yihua Zhang <yihuazhang2@gmail.com>
2022-02-27 17:29:46 -0500

    Update firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -62,7 +62,7 @@ export const addCollectionAndDocuments = async (
 };
 
 export const getCategoriesAndDocuments = async () => {
-  const collectionRef = collection(db, 'categories);
+  const collectionRef = collection(db, 'categories');
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
