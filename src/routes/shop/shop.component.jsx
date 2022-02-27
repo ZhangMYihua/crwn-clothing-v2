654b2e88f2683caac3c5cda0244b8acaec05f1b1
Yihua Zhang <yihuazhang2@gmail.com>
2022-02-27 11:53:12 -0500

    remove unneeded productcard

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,5 @@
 import { useContext, Fragment } from 'react';
 
-import ProductCard from '../../components/product-card/product-card.component';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
