1a37a0e5ba340c38f17133d1418b4a8e354fa77a
Yihua Zhang <yihuazhang2@gmail.com>
2022-02-27 11:56:26 -0500

    updating shop container

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, Fragment } from 'react';
+import { useContext } from 'react';
 
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
@@ -10,12 +10,12 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <Fragment>
+    <div className='shop-container'>
       {Object.keys(categoriesMap).map((key) => {
         const products = categoriesMap[key];
         return <CategoryPreview key={key} title={key} products={products} />;
       })}
-    </Fragment>
+    </div>
   );
 };
 
