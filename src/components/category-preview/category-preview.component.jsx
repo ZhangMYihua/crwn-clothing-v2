24465736665590a03ac2a7ef03a8c98eea55b2e5
Yihua Zhang <yihuazhang2@gmail.com>
2022-02-27 20:56:19 -0500

    created nested routes for shop, fixed styling clash and improved solution

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import ProductCard from '../product-card/product-card.component';
 
 import './category-preview.styles.scss';
@@ -6,7 +8,9 @@ const CategoryPreview = ({ title, products }) => {
   return (
     <div className='category-preview-container'>
       <h2>
-        <span className='title'>{title.toUpperCase()}</span>
+        <Link className='title' to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className='preview'>
         {products
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,16 @@
-import { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className='shop-container'>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
