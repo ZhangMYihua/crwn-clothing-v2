72b402e766c6c4a88e4c084fda22758a1e4f8f28
Yihua Zhang <yihuazhang2@gmail.com>
2022-03-14 23:00:58 -0400

    reverting categories reducer

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,41 +1,13 @@
-import { createContext, useEffect, useReducer } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import { createAction } from '../utils/reducer/reducer.utils';
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
-const CATEGORIES_ACTION_TYPE = {
-  SET_CATEGORIES_MAP: 'SET_CATEGORIES_MAP',
-};
-
-const INITIAL_STATE = {
-  categoriesMap: {},
-};
-
-const categoriesReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case CATEGORIES_ACTION_TYPE.SET_CATEGORIES_MAP:
-      return { ...state, categoriesMap: payload };
-    default:
-      throw new Error(`Unhandled type ${type} in categoriesReducer`);
-  }
-};
-
 export const CategoriesProvider = ({ children }) => {
-  const [{ categoriesMap }, dispatch] = useReducer(
-    categoriesReducer,
-    INITIAL_STATE
-  );
-
-  const setCategoriesMap = (categoriesMap) =>
-    dispatch(
-      createAction(CATEGORIES_ACTION_TYPE.SET_CATEGORIES_MAP, categoriesMap)
-    );
+  const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
     const getCategoriesMap = async () => {
