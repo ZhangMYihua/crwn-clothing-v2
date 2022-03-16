e8bd50cea093ef6deb60950fbb0e35c2f3de4e37
Yihua Zhang <yihuazhang2@gmail.com>
2022-03-16 18:10:00 -0400

    merging with user reducer changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
 import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
 
@@ -16,11 +15,9 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </UserProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
