bde9475db2b941c45cc6787b8ed51f4c641683de
Yihua Zhang <yihuazhang2@gmail.com>
2022-03-27 13:03:49 -0400

    remvoving old console log

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -4,15 +4,6 @@ import { createAction } from '../../utils/reducer/reducer.utils';
 export const setCurrentUser = (user) =>
   createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 
-/*
-CHECK_USER_SESSION: 'user/CHECK_USER_SESSION',
-  GOOGLE_SIGN_IN_START: 'user/GOOGLE_SIGN_IN_START',
-  EMAIL_SIGN_IN_START: 'user/EMAIL_SIGN_IN_START',
-  SIGN_IN_SUCCESS: 'user/SIGN_IN_SUCCESS',
-  SIGN_IN_FAILURE: 'user/SIGN_IN_FAILURE'
-
-*/
-
 export const checkUserSession = () =>
   createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
 
