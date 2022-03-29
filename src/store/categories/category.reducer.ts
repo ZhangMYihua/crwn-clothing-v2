27722c05df9d9e6e132b6ebe037fe95bfb793714
Yihua Zhang <yihuazhang2@gmail.com>
2022-03-29 10:49:00 -0400

    further simplifying withMatcher

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,7 +1,7 @@
+import { AnyAction } from 'redux';
 import { Category } from './category.types';
 
 import {
-  CategoryAction,
   fetchCategoriesStart,
   fetchCategoriesSuccess,
   fetchCategoriesFailed,
@@ -21,7 +21,7 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
+  action = {} as AnyAction
 ): CategoriesState => {
   if (fetchCategoriesStart.match(action)) {
     return { ...state, isLoading: true };
