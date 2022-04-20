6af8c6bf9748d1073a37e51c8cad54515f012c02
georgebrianb <george_brianb@yahoo.com>
2022-04-20 17:38:12 +0300

    working on redux-thunk

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -1,6 +1,8 @@
 import CATEGORIES_ACTION_TYPES from "./categories.types";
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null,
 };
 
 export const categoriesReducer = (
@@ -10,8 +12,12 @@ export const categoriesReducer = (
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categories: payload };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+      return { ...state, isLoading: true };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+      return { ...state, categories: payload, isLoading: false };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAIL:
+      return { ...state, error: payload, isLoading: false };
     default:
       return state;
   }
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
@@ -16,9 +17,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_DEV === "development" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_DEV === "development" && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
