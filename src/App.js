885c7909e2b447469afae1873994b41363ce5c1d
dshaby <daniel@businessesoftheearth.org>
2022-06-02 14:48:58 -0700

    testing first commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 const App = () => {
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
+    <div className="categories-container">
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+        <div className="category-body-container">
+          <h2 className="category-title">Hats</h2>
+          <p className="Shop-now">SHOP NOW</p>
         </div>
       </div>
-      <div className='category-container'>
+
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
+        <div className="category-body-container">
+          <h2 className="category-title">Jackets</h2>
+          <p className="Shop-now">SHOP NOW</p>
         </div>
       </div>
-      <div className='category-container'>
+
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
+        <div className="category-body-container">
+          <h2 className="category-title">Sneakers</h2>
+          <p className="Shop-now">SHOP NOW</p>
         </div>
       </div>
-      <div className='category-container'>
+
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
+        <div className="category-body-container">
+          <h2 className="category-title">Womens</h2>
+          <p className="Shop-now">SHOP NOW</p>
         </div>
       </div>
-      <div className='category-container'>
+
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
+        <div className="category-body-container">
+          <h2 className="category-title">Mens</h2>
+          <p className="Shop-now">SHOP NOW</p>
         </div>
       </div>
     </div>
