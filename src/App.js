8072703d273f7acc9fc8de2f8321ab21e3c67921
Jake Pan <jiaxing1222@gmail.com>
2022-06-04 03:05:21 +0800

    refactored categories

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
+import categories from "./constants/categories"
+import "./categories.styles.scss"
+
 const App = () => {
+
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+    <div className="categories-container">
+
+      {categories.map(({title, id})=> {
+        return (
+          <div className="category-container" key={id}>
+            <div className="background-image"/>
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>)
+      })}
     </div>
   );
 };
