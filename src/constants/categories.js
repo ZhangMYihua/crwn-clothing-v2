4efdec3a3254388e3c00910aa88acb27128ac140
Jake Pan <jiaxing1222@gmail.com>
2022-06-04 03:05:39 +0800

    separated categories

---
+++ src/constants/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+  {
+  id: 1,
+  title: "Hats"
+  },
+  {
+  id: 2,
+  title: "Jackets"
+  },
+  {
+  id: 3,
+  title: "Sneakers"
+  },
+  {
+  id: 4,
+  title: "Womens"
+  },
+  {
+  id: 5,
+  title: "Mens"
+  },
+]
+
+export default categories
