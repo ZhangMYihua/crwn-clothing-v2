8e4118eeecbe586b64b41b2e160f8edff112bda7
Prabodha Jayawardena <prabodha@dev.bukflights.com>
2022-06-06 22:04:55 +0530

    🚧  added sign in with redirect google

--- src/routes/SignIn/SignIn.jsx
+++ src/routes/SignIn/SignIn.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.js';
+import React, { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase.js';
 const SignIn = () => {
 	const logGoogleUser = async () => {
 		const response = await signInWithGooglePopup();
-		console.log(response);
 		await createUserDocumentFromAuth(response.user);
 	};
+	// const logGoogleRedirectUser = async () => {
+	// 	const response = await signInWithGoogleRedirect();
+	// 	//we wont see a console.log becase google will redirect without thinking about our app state
+	// 	//so this logGoogleRedirectUser will not be trigger when this happens
+	// 	console.log(response.user);
+	// 	// await createUserDocumentFromAuth(response.user);
+	// };
+
+	async function redirectResultFromGoogle() {
+		//auth will track of all the authentication regardless where our app is going
+		const result = await getRedirectResult(auth);
+
+		if (result) {
+			await createUserDocumentFromAuth(result.user);
+		}
+	}
+	useEffect(() => {
+		redirectResultFromGoogle();
+	});
 	return (
 		<>
 			<div>SignIn</div>
 			<button onClick={logGoogleUser}>SignIn with google</button>
+			<button onClick={signInWithGoogleRedirect}>SignIn with google redirect</button>
 		</>
 	);
 };
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -17,20 +17,19 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
 	prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) => {
 	const userDocRef = doc(db, 'users', userAuth.uid);
 	const userSnapShot = await getDoc(userDocRef);
-	console.log(userSnapShot);
-	console.log(userSnapShot.exists());
 	//check if userdata exists
 	if (!userSnapShot.exists()) {
 		const { displayName, email } = userAuth;
