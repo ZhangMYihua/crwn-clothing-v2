db8b4ae5fe8a4882b4f912227dd1abbe851c5f7a
Teme Sahle <tsahle@terradatum.com>
2022-06-09 12:58:04 -0400

    add bg images to catagories array

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import Directory from './components/directory/directory.component';
 const App = () => {
   const categories = [
     {
-      id: 1,
-      title: 'hats',
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'jackets',
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 3,
-      title: 'sneakers',
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 4,
-      title: 'womens',
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 5,
-      title: 'mens',
-    },
-  ];
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ];  
 
   return <Directory categories={categories} />;
 };
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -2,10 +2,10 @@ import './category-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
+
   return (
     <div className='category-container'>
-      <div
-        className='background-image'
+      <div className='background-image'
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
