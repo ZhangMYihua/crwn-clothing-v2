c52746a5e70a6d0849f6df29565125eabfbf165d
Prabodha Jayawardena <prabodha@dev.bukflights.com>
2022-06-09 12:26:48 +0530

    ✨  finalizing our website

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { BackgroundImage, Body, CategoryContainer } from './CategoryItemStyle';
 const CategoryItem = (props) => {
 	const { category } = props;
+	const navigate = useNavigate();
+
+	const onNavigateHandler = () => navigate(category.route);
 	return (
-		<CategoryContainer>
+		<CategoryContainer onClick={onNavigateHandler}>
 			<BackgroundImage imageUrl={category.imageUrl}></BackgroundImage>
 			<Body>
 				<h2>{category.title}</h2>
--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import CategoryItem from '../CategoryItem/CategoryItem';
 import './Directory.scss';
+const categories = [
+	{
+		id: 1,
+		title: 'hats',
+		imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+		route: 'shop/hats',
+	},
+	{
+		id: 2,
+		title: 'jackets',
+		imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+		route: 'shop/jackets',
+	},
+	{
+		id: 3,
+		title: 'sneakers',
+		imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+		route: 'shop/sneakers',
+	},
+	{
+		id: 4,
+		title: 'womens',
+		imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+		route: 'shop/womens',
+	},
+	{
+		id: 5,
+		title: 'mens',
+		imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+		route: 'shop/mens',
+	},
+];
 const Directory = (props) => {
-	const { categories } = props;
 	return (
 		<div className='directory-container'>
 			{categories.map((category) => {
