a411b51c7935601c8a4f3a8889ca0dbea03d11a3
Aadi <70697442+Aadi2006@users.noreply.github.com>
2022-06-14 17:03:13 +0530

    This is cool

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 const App = () => {
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
+    <div className="categories-container">
+      <div className="category-container">
+        {console.log("new")}
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Hats</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Jackets</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Sneakers</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Womens</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Mens</h2>
           <p>Shop Now</p>
         </div>
