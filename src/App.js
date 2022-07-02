5110d49c88d53da6f3419623998fb23e47a71680
Martin <haenschmartin@gmail.com>
2022-07-01 23:40:44 -0700

    Outsourcing categories

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 1,
+      title: "Jackets",
+    },
+    {
+      id: 1,
+      title: "Sneakers",
+    },
+    {
+      id: 1,
+      title: "Womens",
+    },
+    {
+      id: 1,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          {/* <img /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
