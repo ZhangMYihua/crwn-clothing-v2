dc214a8aa417152b2b54c774884119558d6daa2e
Martin <haenschmartin@gmail.com>
2022-07-02 10:57:23 -0700

    outsoucing category-componente

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
+import CategoryItem from "./components/category-item/category-item.component";
+
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Hats",
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      id: 1,
-      title: "Jackets",
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      id: 1,
-      title: "Sneakers",
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      id: 1,
-      title: "Womens",
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      id: 1,
-      title: "Mens",
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          {/* <img /> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
