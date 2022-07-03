cdbc77530125c7aebfd3f1886b8461625fc492d7
Pawe Tylczyski <pawel.tylczynski@gmail.com>
2022-07-03 12:32:01 +0200

    Eslint configuration, category page refactor

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
+import React from 'react';
+import './categories.styles.scss';
+
 const App = () => {
-  return (
-    <div className='categories-container'>
-      <h2>Clothing</h2>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+
+    const categories = [
+        {
+            id: 1,
+            title: 'Hats',
+        },
+        {
+            id: 2,
+            title: 'Jackets',
+        },
+        {
+            id: 3,
+            title: 'Sneakers',
+        },
+        {
+            id: 4,
+            title: 'Womens',
+        },
+        {
+            id: 5,
+            title: 'Mens',
+        }
+    ];
+
+
+    return (
+        <div className='categories-container'>
+            {categories.map(({id, title}) => (
+                <div className='category-container' key={id}>
+                    <div className='background-image' />
+                    <div className='category-body-container'>
+                        <h2>{title}</h2>
+                        <p>Shop Now</p>
+                    </div>
+                </div>
+            ))}
         </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default App;
