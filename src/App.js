eacde97f2cd2202ed814c25300db01fbed2ea09e
Martin <haenschmartin@gmail.com>
2022-07-03 22:58:28 -0700

    Adding Navbar to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" index element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navivation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
