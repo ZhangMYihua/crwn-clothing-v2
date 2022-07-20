f2279c835797b32656164cf13889ace8ffd27db1
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-07-20 09:57:20 -0400

    addded directory, and category item pages and integrated sass with stylesheets

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CategoryItem from '../category-item/category-item.component';
+
+import './directory.styles.scss';
+
+export const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
