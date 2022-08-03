fb9732da61aaadf0cf4498aca5e8f28fcfd9850c
OliverTang <snakeystart@gmail.com>
2022-08-03 18:55:17 -0400

    for test

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,7 +1,7 @@
-import './category-item.styles.scss';
+import './category-item.styles.scss'
 
 const CategoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title } = category
   return (
     <div className='category-container'>
       <div
@@ -12,10 +12,11 @@ const CategoryItem = ({ category }) => {
       />
       <div className='category-body-container'>
         <h2>{title}</h2>
+        <p>for test</p>
         <p>Shop Now</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CategoryItem;
+export default CategoryItem
