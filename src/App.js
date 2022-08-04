5ac5116bb7a7de1476136453d7e3a5ace514bb39
Vivek <vivek_wolves@yahoo.com>
2022-08-03 22:42:46 -0700

    Section4-79 Category Directory

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import "./categories.styles.scss";
 import "../src/components/category-item/category-item.component.js";
 import CategoryItem from "../src/components/category-item/category-item.component.js";
+import CategoryDirectory from "./components/category-directory/category-directory.component";
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category}></CategoryItem>
-      ))}
-    </div>
-  );
+  return <CategoryDirectory></CategoryDirectory>;
 };
 
 export default App;
