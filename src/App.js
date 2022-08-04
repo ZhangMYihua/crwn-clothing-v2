c3870ae5372dde245dcf900b3a3b4ddceb6149c4
Vivek <vivek_wolves@yahoo.com>
2022-08-03 22:14:09 -0700

    Section4-77 Sass

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./categories.styles.scss";
 const App = () => {
   const categories = [
     {
@@ -23,8 +24,8 @@ const App = () => {
   ];
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
+      {categories.map(({ id, title }) => (
+        <div key={id} className="category-container">
           <div className="background-image"></div>
           <div className="category-body-container">
             <h2>{title}</h2>
