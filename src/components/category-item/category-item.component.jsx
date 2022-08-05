30391e7df38a18f53fb65cb7e82b7a86a4969d5a
Issam O <issamoutassourt@gmail.com>
2022-08-05 06:21:59 +0100

    corrected id import since it happens in parent component

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,9 +1,9 @@
 import './category-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
-  const { id, imageUrl, title } = category;
+  const { imageUrl, title } = category;
   return (
-    <div key={id} className='category-container'>
+    <div className='category-container'>
       <div
         className='background-image'
         style={{
