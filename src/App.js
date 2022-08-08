036c01b73b6ce08a1485ce5e1ccafcb988ad067c
Vivek <vivek_wolves@yahoo.com>
2022-08-07 19:05:03 -0700

    Section4- 80 Fonts

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ import Directory from "./components/category-directory/directory.component";
 const categories = [
   {
     id: 1,
-    title: "hats",
+    title: "HATS",
     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
   },
   {
     id: 2,
-    title: "jackets",
+    title: "JACKETS",
     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
   },
   {
     id: 3,
-    title: "sneakers",
+    title: "SNEAKERS",
     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
   },
   {
     id: 4,
-    title: "womens",
+    title: "WOMENS",
     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
   },
   {
     id: 5,
-    title: "mens",
+    title: "MENS",
     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
   },
 ];
