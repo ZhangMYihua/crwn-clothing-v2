6ee75fc3ba59f92f5a4cf88ccdcc5de0549ff39c
Nnaemeka Anthony Eneh <108509845+anthonyeneh@users.noreply.github.com>
2022-08-12 21:06:43 +0100

    added navigation bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,8 @@
+const SignIn = () => {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+    </div>
+  );
+};
+export default SignIn;
