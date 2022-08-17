04e9da8c905ad6dbff387b27d6477d27a79f1640
Nathaniel <nielb4all@gmail.com>
2022-08-16 20:42:44 -0700

    index.js code cleanup

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,2 +1,15 @@
 import { createContext } from "react";
 
+//as the actual value you want to access
+export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null,
+    
+});
+
+export const UserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const value = { currentUser, setCurrentUser };
+  return;
+  <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.scss";
 
-ReactDOM.render(
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
@@ -13,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
