7d687c42dcf5a512817e821912c7da2b9c59d902
Patrik Karlsson <crossfithisingen@gmail.com>
2022-08-22 10:11:00 +0200

    modulized data into directory component and UI into category-item component

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,18 +1,18 @@
-// import './category-item.style.scss';
+import './category-item.style.scss';
 
-// const CategoryItem = ({ category }) => {
-//   const { imageUrl, title } = category;
-//   return (
-//     <div className="category-container">
-//       <div
-//         className="background-image"
-//         style={{ backgroundImage: `url(${imageUrl})` }}
-//       />
-//       <div className="category-body-container">
-//         <h2>{title}</h2>
-//         <p>Shop Now</p>
-//       </div>
-//     </div>
-//   );
-// };
-// export default CategoryItem;
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+export default CategoryItem;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,10 +2,12 @@ import CategoryItem from '../category-item/category-item.component';
 import './directory.styles.scss';
 
 const Directory = ({ categories }) => {
-  <div className="directory-container">
-    {categories.map((category) => (
-      <CategoryItem key={category.id} category={category} />
-    ))}
-  </div>;
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
 };
 export default Directory;
