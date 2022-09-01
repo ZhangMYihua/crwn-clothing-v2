140080b2f51ce3d45ca5656f187e27f1bd569812
Nnaemeka Anthony Eneh <108509845+anthonyeneh@users.noreply.github.com>
2022-09-01 10:41:49 -0400

    new features

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,23 +1,21 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import { CartContext } from '../../contexts/cart.context';
-
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
-} from './cart-dropdown.styles';
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
-    navigate('/checkout');
+    navigate("/checkout");
   };
 
   return (
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
