af9748334a9666678a93cbc8fa40c1e19ea7b0bf
barath <barath@joinassembly.com>
2022-09-05 00:03:44 +0530

    - added navbar component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/navigation/index.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./styles.scss";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
