b053d37c16a05af74e0bca23fff7be87d6518141
Patrik Karlsson <crossfithisingen@gmail.com>
2022-09-04 11:44:50 +0200

    lesson 8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Nav from './routes/navigation/nav.component';
+import SignIn from './routes/sign-in/sign-In.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -11,7 +11,7 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path="/" element={<Nav />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="sign-in" element={<SignIn />} />
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -6,7 +6,9 @@ const CategoryItem = ({ category }) => {
     <div className="category-container">
       <div
         className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
       />
       <div className="category-body-container">
         <h2>{title}</h2>
@@ -15,4 +17,5 @@ const CategoryItem = ({ category }) => {
     </div>
   );
 };
+
 export default CategoryItem;
