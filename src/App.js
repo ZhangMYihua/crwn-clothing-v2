0e3e5670ed4284e78b4b1ae3c5d06b466f0312db
Robert S <robscafe333@yahoo.com>
2022-09-05 14:16:14 -0700

    changed hard-coded image line 28 to a div for purpose of dynamically displaying image, also added npm sass

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     <div className="categories-container">
       {categories.map(({ title }) => (
         <div className="category-container">
-          {/* <img src="" alt="" /> */}
+          <div className="background-image"></div>
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop now</p>
