be0ed641a40c440e05e55a997cc58995989fc99f
Robert S <robscafe333@yahoo.com>
2022-09-05 13:50:49 -0700

    first html code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
