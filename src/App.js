45aa0fa6b26e44cf71959e5431d1319fde1949a3
Robert S <robscafe333@yahoo.com>
2022-09-05 14:05:00 -0700

    use of components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hat",
+    },
+  ];
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop now</p>
+      {categories.map((title) => (
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop now</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
