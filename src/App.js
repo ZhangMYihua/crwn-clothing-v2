45173160b28fd094dabd6c12aa6bdb4792354f38
Ahmad Almasri <ahmadmohdalmasri@gmail.com>
2022-09-11 19:36:43 +0200

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import "./categories.styles.scss"
-import Directory from "./components/directory/Directory.component";
+import {Routes, Route } from "react-router-dom";
+import Home from "./components/routes/home/Home.component";
+import Navigation from "./components/routes/navigation/Navigation.component";
+
+
+const Shop = ()=>{
+  return(
+    <div>
+      <h1>this is the shop</h1>
+    </div>
+  )
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  
-  return <Directory categories={categories}/>
-  
-  }
+return(
+  <Routes>
+    <Route path='/' element = {<Navigation/>}>
+      <Route index element = {<Home/>}/>
+      <Route path ='shop' element = {<Shop/>}/>
+    </Route>
+  </Routes>
+)  
+}
+
 export default App;
