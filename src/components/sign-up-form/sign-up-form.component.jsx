9c4c2bdcfa500f1db9b130aa54bfed577c4c2cab
Pouya Elishaee <p.elishaee@gmail.com>
2022-09-13 16:57:55 -0400

    add submit form

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,43 @@
+import { exportDefaultDeclaration } from "@babel/types"
+import { useState } from "react"
+
+const defaultFormFields ={
+    displayName : '' ,
+    email : '',
+    password : '',
+    confirmPassword: '',
+}
+
+const SignUpForm = () => {
+
+    const [formFields,setFormFields] = useState(defaultFormFields)
+    const {displayName,email,password,confirmPassword} = formFields
+
+const handleChange=(event) =>{
+    const {name , value} = event.target ;
+
+    setFormFields({...formFields, [name]:value})
+}
+    
+    return(
+        <div>
+            <h1>Sign up with your email and password</h1>
+            <form onSubmit={()=>{}}>
+                <label >Display Name</label>
+                <input type="text" required onChange={handleChange} name="displayName" value={displayName}/>
+
+                <label >Email</label>
+                <input type="email"required onChange={handleChange} name="email" value={email}/>
+
+                <label >Password</label>
+                <input type="password" required onChange={handleChange} name="password" value={pasword}/>
+
+                <label >Confirm Password</label>
+                <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm
--- src/utils/firebase/firbase.utils.js
+++ src/utils/firebase/firbase.utils.js
@@ -14,13 +14,14 @@ const firebaseConfig = {
   // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({
     prompt: "select_account"
 })
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore();
 
