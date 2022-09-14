1660a50d1296ffb32e4bd0883c2aa104f3885231
barath <barath@joinassembly.com>
2022-09-14 23:02:03 +0530

    - added shop comp

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home";
 import Navigation from "./routes/navigation";
 import Authentication from "./routes/authentication";
+import Shop from "./routes/shop";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route
-          path="shop"
-          element={() => {
-            return <p>Shop</p>;
-          }}
-        />
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
---
+++ src/routes/shop/index.jsx
@@ -0,0 +1,15 @@
+import SHOP_DATA from "../../shopData.json";
+
+const Shop = () => {
+  return (
+    <div>
+      {SHOP_DATA.map(({ id, name }) => (
+        <div key={id}>
+          <h1>{name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Shop;
