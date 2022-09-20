6c9c8cbab6189594bba296b8d300712a96635274
barath <barath@joinassembly.com>
2022-09-20 19:51:09 +0530

    - added style for a comp

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,7 @@ import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
-import "./cart-dropdown.styles.scss";
+import { CartDropdownContainer, CartItems } from "./cart-dropdown.styles.jsx";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -16,8 +16,8 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
+    <CartDropdownContainer>
+      <CartItems>
         {cartItems.length ? (
           cartItems.map((cartItem) => (
             <CartItem key={cartItem.id} cartItem={cartItem} />
@@ -25,9 +25,11 @@ const CartDropdown = () => {
         ) : (
           <span className="empty-message">Your cart is empty</span>
         )}
-      </div>
-      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+      </CartItems>
+      <Button onClick={goToCheckoutHandler} style={{ marginTop: "auto" }}>
+        GO TO CHECKOUT
+      </Button>
+    </CartDropdownContainer>
   );
 };
 
