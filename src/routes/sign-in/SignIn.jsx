05555c6badd5b2d0ef6b383208daecf4065fa37e
Angel Giron <angel.c.giron1@gmail.com>
2022-09-23 22:53:00 -0700

    removed firebase redirect

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,15 +1,36 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase"
+// import {useEffect} from 'react'
+// import { getRedirectResult } from 'firebase/auth'
+
+
+import { 
+  // auth, 
+  signInWithGooglePopup, 
+  // signInWithGoogleRedirect, 
+  createUserDocumentFromAuth } from "../../utils/firebase/firebase"
 
 const SignIn = () => {
+
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth)
+  //   if (response) {
+  //     const userDocRef = await createUserDocumentFromAuth(response.user)  
+  //   }
+  // }, [])
+
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(response.user)
   }
+  // const logGoogleRedirectUser = async () => {
+  //   const response = await signInWithGoogleRedirect();
+  //   console.log(response.user)
+  // }
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser} >Sign in with Google Popup</button>
+      {/* <button onClick={signInWithGoogleRedirect} >Sign in with Google Redirect</button> */}
     </div>
   )
 }
