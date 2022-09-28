03acfd6d0be6ec8237d90640856872b7574ef72e
Pouya Elishaee <p.elishaee@gmail.com>
2022-09-28 16:53:02 -0400

    correct styles

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,13 @@
 import ProductCard from '../product-card/product-card.component'
 import './category-preview.sytles.scss'
+import { Link } from 'react-router-dom';
 
 const CategoryPreview = ({title,products}) => {
     return (
         <div className='category-preview-container'>
             <h2>
-                <span className='title'>{title.toUpperCase()}</span>
+                <Link className='title' to={title}>
+                    {title.toUpperCase()}</Link>
             </h2>
             <div className='preview'>
                 {products
