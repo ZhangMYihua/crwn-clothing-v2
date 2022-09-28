a737eef59ebda1b46763a6672fdd70780dbba7a2
Jairo Matus <jairoamatus@outlook.com>
2022-09-28 04:57:38 -0500

    Woo

--- src/App.js
+++ src/App.js
@@ -3,41 +3,13 @@ const App = () => {
     <div className='categories-container'>
       <div className='category-container'>
         {/* <img /> */}
+        <div />
         <div className='category-body-container'>
           <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
           <p>Shop Now</p>
         </div>
       </div>
     </div>
   );
 };
-
 export default App;
