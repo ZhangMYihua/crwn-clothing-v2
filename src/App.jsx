c37594b05b548cbbabea005f3a9a39322106cb65
Jairo Matus <jairoamatus@outlook.com>
2022-09-28 05:27:59 -0500

    fixing brackets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 const App = () => {
+  const categories = [
+    { 
+      id: 1, 
+      title:'' 
+    },
+    { 
+      id: 2,
+      title:''
+    },
+    { 
+      id: 3,
+       title:''
+    }
+  ]
+
   return (
     <div className='categories-container'>
       <div className='category-container'>
         {/* <img /> */}
         <div />
+        {categories.map(({ title }) => (
         <div className='category-body-container'>
-          <h2>Hats</h2>
+          <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
+          ))}
       </div>
     </div>
   );
