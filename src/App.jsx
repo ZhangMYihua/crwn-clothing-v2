bf77990c94311df90cdd5187ff7f5af00b57aa89
Jairo Matus <jairoamatus@outlook.com>
2022-09-28 06:02:12 -0500

    Adding background img

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,43 @@ import './categories.style.scss'
 
 const App = () => {
   const categories = [
-    { 
-      id: 1, 
-      title:'Hats' 
-    },
-    { 
-      id: 2,
-      title:'Jackets'
-    },
-    { 
-      id: 3,
-       title:'Sneakers'
-    },
-    {
-      id: 4,
-      title: 'Women'
-    },
-    {
-      id: 5,
-      title: 'Men'
-    }
-  ]
+  {
+    "id": 1,
+    "title": "Hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "Jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "Sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "Women",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "Men",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  }
+]
+
 
   return (
     <div className='categories-container'>
-        {categories.map(({ id, title }) => (
+        {categories.map(({ id, title, imageUrl }) => (
       <div className='category-container'>
         {/* <img /> */}
-        <div className='background-image'/>
+        <div className='background-image' style={{
+          backgroundImage: `url(${imageUrl})`
+        }} />
+        
         <div key={id} className='category-body-container'>
           <h2>{title}</h2>
           <p>Shop Now</p>
