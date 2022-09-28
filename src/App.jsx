9edcb64b97c947e6814fdde0184e94d3c85e10d7
Jairo Matus <jairoamatus@outlook.com>
2022-09-28 05:58:35 -0500

    Splitting out cart-item into it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import './'
+import './categories.style.scss'
+
 const App = () => {
   const categories = [
     { 
       id: 1, 
-      title:'' 
+      title:'Hats' 
     },
     { 
       id: 2,
-      title:''
+      title:'Jackets'
     },
     { 
       id: 3,
-       title:''
+       title:'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Women'
+    },
+    {
+      id: 5,
+      title: 'Men'
     }
   ]
 
   return (
     <div className='categories-container'>
+        {categories.map(({ id, title }) => (
       <div className='category-container'>
         {/* <img /> */}
-        <div />
-        {categories.map(({ title }) => (
-        <div className='category-body-container'>
+        <div className='background-image'/>
+        <div key={id} className='category-body-container'>
           <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
-          ))}
       </div>
+      ))}
     </div>
   );
 };
