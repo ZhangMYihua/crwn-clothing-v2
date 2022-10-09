1b166a8d54363e02bf1a4495ac9f8eb6f0ca72e7
Ahmad Almasri <ahmadmohdalmasri@gmail.com>
2022-10-09 22:38:47 +0200

    added redirects fir netlify

--- src/components/directory/Directory.component.jsx
+++ src/components/directory/Directory.component.jsx
@@ -12,7 +12,7 @@ const categories = [
     id: 2,
     title: "jackets",
     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    route: "shop/jkackets"
+    route: "shop/jackets"
   },
   {
     id: 3,
