860a827fe2665e89dc4e2c45a7fc55916fa0594d
Angel Giron <angel.c.giron1@gmail.com>
2022-10-10 11:25:15 -0700

    refactored ProductCard

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,7 +1,7 @@
 import { Button, BUTTON_TYPE_CLASSES } from '../button/Button'
 import { useContext } from 'react'
 import { CartContext } from '../../contexts/CartContext'
-import './product-card.scss'
+import { ProductCardContainer, Footer, Name, Price } from './product-card.styles'
 
 export const ProductCard = ({product, category}) => {
   const { addItemToCart } = useContext(CartContext)
@@ -10,17 +10,17 @@ export const ProductCard = ({product, category}) => {
   const addProductToCart = () => addItemToCart(product)
 
   return (
-    <div className='product-card-container'>
+    <ProductCardContainer>
       <img src={imageUrl} alt={name}/>
-      <div className='footer' >
-        <span className='name' >{name}</span>
-        <span className='price' >{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button 
         buttonType={BUTTON_TYPE_CLASSES.inverted} 
         onClick={addProductToCart}>
         Add to cart
       </Button>
-    </div>
+    </ProductCardContainer>
   )
 }
--- src/components/product-card/product-card.styles.jsx
+++ src/components/product-card/product-card.styles.jsx
@@ -1,11 +1,13 @@
-.product-card-container {
+import styled from 'styled-components'
+
+export const ProductCardContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   height: 350px;
   align-items: center;
   position: relative;
-
+  
   img {
     width: 100%;
     height: 95%;
@@ -31,21 +33,19 @@
       display: flex;
     }
   }
+`
 
-  .footer {
-    width: 100%;
-    height: 5%;
-    display: flex;
-    justify-content: space-between;
-    font-size: 18px;
-
-    .name {
-      width: 90%;
-      margin-bottom: 15px;
-    }
-
-    .price {
-      width: 10%;
-    }
-  }
-}
+export const Footer = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+`
+export const Name = styled.span`
+  width: 90%;
+  margin-bottom: 15px;
+`
+export const Price = styled.span`
+  width: 10%;
+`
