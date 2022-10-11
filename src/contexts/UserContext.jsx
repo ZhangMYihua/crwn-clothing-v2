28dac3cf7408c8f36974a1cc970ca989f64c66f4
Angel Giron <angel.c.giron1@gmail.com>
2022-10-11 08:15:13 -0700

    added useReducer in UserContext

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react"
+import { createContext, useState, useEffect, useReducer } from "react"
 // think of useContext as a glorified storage component that is leveraging useState. We are exposing the value and setter function of this context externally.
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase";
 
@@ -11,9 +11,44 @@ export const UserContext = createContext({
   setCurrentUser: () => null
 })
 
+// USE REDUCER
+export const USER_ACTION_TYPES = {
+  "SET_CURRENT_USER": "SET_CURRENT_USER"
+}
+
+const userReducer = (state, action) => {
+  const {type, payload} = action;
+  
+  switch(type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload
+      }
+    default: 
+      throw new Error(`Unhandled type ${type} in userReducer`)
+  }
+}
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
 // this is the component we'll use in our app that will allow any of its children to access the values of its useState 
 export const UserProvider = ({children}) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  // useReducer takes two args, a reducer and the initial value for the state
+  // the dispatch function receives an action object, and then passes it into userReducer
+  const [ state, dispatch ] = useReducer(userReducer, INITIAL_STATE)
+
+  const { currentUser } = state;
+  const setCurrentUser = (user) => {
+    dispatch({ 
+      type: USER_ACTION_TYPES.SET_CURRENT_USER,
+      payload: user
+    })
+  }
+
   const value = {currentUser, setCurrentUser}
   
   useEffect(() => {
@@ -31,3 +66,12 @@ export const UserProvider = ({children}) => {
     </UserContext.Provider>
   )
 }
+
+/**
+ * reducers are functions that return objects
+ * const userReducer = (state, action) => {
+ *  return {
+ *    currentUser: {...}
+ *  }
+ * }
+ */
