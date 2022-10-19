aa6bb0fe9df5546df57d88767976a5407d1c846e
ahmadDev <ahmadjafari6050@gmail.com>
2022-10-19 22:25:50 +0330

    feat:  sign In With Google Redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,19 @@
-import { signInWithGooglePopup, userDocFromAuth } from "../../utils/firebase/firebase.utils"
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, userDocFromAuth } from "../../utils/firebase/firebase.utils"
+
 const SignIn = () => {
+    useEffect ( () => {
+        async function fetchData() {
+            const response = await getRedirectResult(auth);
+            if (response) {
+              const userDocRef = await userDocFromAuth(response.user);
+              console.log("userDocRef: ", userDocRef);
+            }
+        }
+        fetchData();
+    }, [])
+    
     const signInGoogle = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await userDocFromAuth(user);
@@ -9,6 +23,7 @@ const SignIn = () => {
         <div>
             <h1>sign in page</h1>
             <button onClick={signInGoogle}>Google login</button>
+            <button onClick={signInWithGoogleRedirect}>Google login Redirect</button>
         </div>
       );
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -15,13 +15,14 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore();
 export const userDocFromAuth = async (userAuth) => {
