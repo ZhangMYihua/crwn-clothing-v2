8e7935001805e0677bea18e4c262bd46212a8efe
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2022-10-20 22:03:09 -0300

    crw-02 - creating an user document based on the authenticated user

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
   GoogleAuthProvider 
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDlgwDQ0QmfqXvJhcxgBl7cERVUHHpneRg",
   authDomain: "crwn-clothing-db-fb8ff.firebaseapp.com",
@@ -16,7 +23,6 @@ const firebaseConfig = {
   measurementId: "G-GYSRR0STE0"
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
@@ -26,3 +32,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch(err) {
+      console.log('error creating the user', err.message);
+    }
+  }
+
+  return userDocRef;
+};
