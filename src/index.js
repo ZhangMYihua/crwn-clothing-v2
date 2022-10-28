6bf17378943fed684a251acab79fd79d7ca41d53
ahmadDev <ahmadjafari6050@gmail.com>
2022-10-28 16:06:10 +0330

    product card

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { UserProvider } from './context/user.context';
+import { ProductsProvider } from './context/products.context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,14 @@
-import SHOP_DATA from "../../shop-data.json";
+import { useContext } from "react";
+import { ProductsContext } from "../../context/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import './shop.styles.scss'
 
 const Shop = () => {
+  const {products} = useContext(ProductsContext);
     return ( 
-        <div>
-            {SHOP_DATA.map((shop) => {
-              return <h3>{shop.name}</h3>  
+        <div className="products-container">
+            {products.map((product) => {
+              return <ProductCard key={product.id} product={product} />
             })}
         </div>
      );
