7f576ac9b52fb84b16971dc788910e68351b1440
ahmadDev <ahmadjafari6050@gmail.com>
2022-11-01 23:55:22 +0330

    button styled component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,15 +1,22 @@
-import "./button.styles.scss";
+import { BaseButton, GoogleSignInButton, InvertedButton} from "./button.styles";
+
 const BUTTON_TYPES_CLASSES = {
+    base: "base",
     google: "google-sign-in",
     inverted:  'inverted',
 }
 
+const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) => (
+        {
+            [BUTTON_TYPES_CLASSES.base]: BaseButton,
+            [BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
+            [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
+        }[buttonType]
+    )
+
 const Button = ({children, buttonType, ...otherProps}) => {
-    return ( 
-        <button className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`} {...otherProps}>
-            {children}
-        </button>
-     );
+    const CustomButton = getButton(buttonType);
+    return (<CustomButton {...otherProps}>{children}</CustomButton>);
 }
 
 export default Button;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,33 +1,33 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../context/user.context";
 import { CartContext } from "../../context/cart.context";
 import { userSignOut } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import {} from "./navigation.styles"
+import { NavigationContainer, LogoContainer, NavLinks, NavLink, } from "./navigation.styles"
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
     const {isCartOpen} = useContext(CartContext);
     return ( 
         <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to="/">
+            <NavigationContainer>
+                <LogoContainer to="/">
                     <CrownLogo className="logo"/>
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to="/shop">SHOP</Link>
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to="/shop">SHOP</NavLink>
                     {
                         currentUser ? 
-                        (<span className="nav-link" onClick={userSignOut}>SIGN OUT</span>) :
-                        (<Link className="nav-link" to="/auth">SIGN IN</Link>)
+                        (<NavLink as="span" onClick={userSignOut}>SIGN OUT</NavLink>) :
+                        (<NavLink to="/auth">SIGN IN</NavLink>)
                     }
                     <CartIcon/>
-                </div>
+                </NavLinks>
                 { isCartOpen && <CartDropdown/> }
-            </div>
+            </NavigationContainer>
             <Outlet/>
         </Fragment>
      );
