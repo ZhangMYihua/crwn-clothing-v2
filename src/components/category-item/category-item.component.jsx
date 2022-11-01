5a09c923c03b4f9946cfb0b4275ff2d41581970d
ahmadDev <ahmadjafari6050@gmail.com>
2022-11-01 22:47:48 +0330

    category preview done

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -3,14 +3,14 @@ import "./category-item.styles.scss"
 const CategoryItem = ({category}) => {
     const { imageUrl, title } = category;
     return (  
-        <div className='category-container'>
-        <div className="background-image" style={{
-          backgroundImage:`url(${imageUrl})`,
-        }}/>
-        <div className='category-body-container'>
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
+        <div className='category-item-container'>
+          <div className="background-image" style={{
+            backgroundImage:`url(${imageUrl})`,
+          }}/>
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
       </div>
     );
 }
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -11,10 +11,15 @@ const Category = () => {
     useEffect(() => {
         setProducts(categoriesMap[category])
     }, [category, categoriesMap])
-    return ( <div className="category-container">
-        {products && 
-        products.map((product) => <ProductCard key={product.id} product={product} />)}
-    </div> );
+    return (
+        <>
+            <h2 className="category-title">{category.toUpperCase()}</h2>
+            <div className="category-container">
+                {products && 
+                products.map((product) => <ProductCard key={product.id} product={product} />)}
+            </div> 
+        </> 
+    );
 }
 
 export default Category;
