08b17998c7ea2ec38f591f2cbe7dd220b01c6bf9
ahmadDev <ahmadjafari6050@gmail.com>
2022-11-03 00:13:49 +0330

    fix: warning

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,6 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../utils/firebase/firebase.utils"
-import SHOP_DATA from "../shop-data.js";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils"
 
 export const CategoriesContext = createContext({
     categoriesMap: {},
@@ -8,9 +7,6 @@ export const CategoriesContext = createContext({
 
 export const CategoriesProvider = ({children}) => {
     const [categoriesMap, setCategoriesMap] = useState({});
-    // useEffect(() => {
-    //     addCollectionAndDocuments("categories", SHOP_DATA);
-    // }, [])
     useEffect(() => {
         const getCategoriesMap = async() => {
           const categoriesMap =  await getCategoriesAndDocuments();
