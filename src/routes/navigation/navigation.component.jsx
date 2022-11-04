4e561c7120cd1598a66fdccbf86949a58d8b2a66
ahmadDev <ahmadjafari6050@gmail.com>
2022-11-04 15:39:20 +0330

    categories store

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,8 @@
 import { Fragment, useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../context/user.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
 import { CartContext } from "../../context/cart.context";
 import { userSignOut } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
@@ -9,7 +10,7 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 import { NavigationContainer, LogoContainer, NavLinks, NavLink, } from "./navigation.styles"
 
 const Navigation = () => {
-    const {currentUser} = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser);
     const {isCartOpen} = useContext(CartContext);
     return ( 
         <Fragment>
---
+++ src/store/user/user.selector.js
@@ -0,0 +1,2 @@
+
+export const selectCurrentUser = (state) => state.user.currentUser;
