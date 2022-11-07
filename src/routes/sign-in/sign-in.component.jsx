8e8ef63111cf69fbed9d003b6bf5d579a6d2e68f
Sandile Biyela <Sandile.Biyela@media24.com>
2022-11-07 12:04:51 +0200

    initial commit with firebase auth added

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,21 @@
 import {
-  signInWithGooglePopup,
-  createUserProfileDocument,
-} from '../../utils/firebase/firebase.utils';
+	signInWithGooglePopup,
+	createUserProfileDocument,
+	createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    createUserProfileDocument(response);
-  };
+	const logGoogleUser = async () => {
+		const { user } = await signInWithGooglePopup();
+		const userDocRef = await createUserDocumentFromAuth(user);
+	};
 
-  return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Sign In Page</h1>
+			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
+		</div>
+	);
 };
 
 export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,18 +1,20 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
-  getAuth,
-  signInWithRedirect,
-  signInWithPopup,
-  GoogleAuthProvider,
-} from 'firebase/auth';
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+	apiKey: "AIzaSyBOtm2sxC_vO9v4omJU_f6VGKloHyaOeTg",
+	authDomain: "rag-sauls-db.firebaseapp.com",
+	projectId: "rag-sauls-db",
+	storageBucket: "rag-sauls-db.appspot.com",
+	messagingSenderId: "829162064958",
+	appId: "1:829162064958:web:abdd001851139cd9fc156f",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -20,14 +22,40 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-  prompt: 'select_account',
+	prompt: "select_account",
 });
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+// export const createUserProfileDocument = async (userAuth, additionalData) => {
+// 	if (!userAuth) return;
 
-  console.log(userAuth);
-};
+// 	console.log(userAuth);
+// };
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log("error creating the user", error.message);
+		}
+	}
+};
