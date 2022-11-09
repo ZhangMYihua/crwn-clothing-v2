10869468f5d0326736b854aeac72da1b9c81f266
ashwin alexander <alexwin75@gmail.com>
2022-11-08 20:49:40 -0500

    setting up Stripe

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,29 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    //no typical form submission behaviour
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+  };
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit Card Payment</h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
@@ -16,7 +18,9 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
