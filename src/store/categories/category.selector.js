28d5eb0fb1979d68ddd78a1276bc19498737e905
ahmadDev <ahmadjafari6050@gmail.com>
2022-11-12 00:22:27 +0330

    categories go to redux

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) => 
-    state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer], 
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => 
+    categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
     }, {})
+)
