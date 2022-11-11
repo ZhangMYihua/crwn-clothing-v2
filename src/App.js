5ad464f974a493c5fc857c4780317fff6ef06e00
Sandile Biyela <Sandile.Biyela@media24.com>
2022-11-11 11:02:46 +0200

    created authentication and sign in form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
-  return <h1>I am the shop page</h1>;
+	return <h1>I am the shop page</h1>;
 };
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path="shop" element={<Shop />} />
+				<Route path="auth" element={<Authentication />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,93 @@
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import { useState } from "react";
+import {
+	createAuthUserWithEmailAndPassword,
+	createUserDocumentFromAuth,
+	signInAuthUserWithEmailAndPassword,
+	signInWithGooglePopup,
+} from "../../utils/firebase/firebase.utils";
+
+import "./sign-in-form.styles.scss";
+
+const defaultFormFields = {
+	email: "",
+	password: "",
+};
+
+const SignInForm = () => {
+	const [formFields, setFormFields] = useState(defaultFormFields);
+	const { email, password } = formFields;
+
+	const signInWithGoogle = async () => {
+		const { user } = await signInWithGooglePopup();
+		const userDocRef = await createUserDocumentFromAuth(user);
+		console.log(userDocRef);
+	};
+
+	const resetFormFields = () => {
+		setFormFields(defaultFormFields);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		try {
+			const response = await signInAuthUserWithEmailAndPassword(
+				email,
+				password
+			);
+			console.log(response);
+		} catch (error) {
+			if (error) {
+				alert(error.code.split("/")[1]);
+			}
+		}
+	};
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setFormFields({ ...formFields, [name]: value });
+	};
+	return (
+		<div className="sign-in-container">
+			<h2>Already have an account?</h2>
+			<span>Sign in with your email and password</span>
+			<form onSubmit={handleSubmit}>
+				<FormInput
+					label="Email"
+					type="email"
+					required
+					onChange={handleChange}
+					name="email"
+					value={email}
+				/>
+
+				<FormInput
+					label="Password"
+					type="password"
+					required
+					onChange={handleChange}
+					name="password"
+					value={password}
+				/>
+				<div className="buttons-container">
+					<Button type="submit">Sign In</Button>
+
+					{/* button component doesn't work for the google button */}
+					<button
+						onClick={signInWithGoogle}
+						className="button-container google-sign-in"
+						type="button"
+					>
+						Google Sign In
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default SignInForm;
