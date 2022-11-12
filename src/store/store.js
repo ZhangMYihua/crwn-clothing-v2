50af171e9b1e58fcd532e1dd952a42b7ded5d7ad
ahmadDev <ahmadjafari6050@gmail.com>
2022-11-12 23:19:45 +0330

    use redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -15,9 +15,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
+const middleWares = [ process.env.NODE_ENV !== "production" && logger].filter(Boolean);
 
-const middleWares = [logger];
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer = (
+    process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+) || compose
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
 
