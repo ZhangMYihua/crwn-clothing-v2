e890000da051daf8b877c0c525f4d705d7fad330
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2022-11-16 17:07:15 -0300

    crw-03 - adding shop.component with basic product-cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return (
-    <h1>I'm the shop page</h1>
-  );
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
