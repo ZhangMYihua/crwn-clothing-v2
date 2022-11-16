e3b8e606aaee52e595f7dc9f656fb4348542176c
Oscar Delgado <oscardel413@gmail.com>
2022-11-15 23:19:05 -0700

    update sign-in url to auth

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Route path='/' element={<Navigation/>}>
         <Route index element={<Home/>}/>
         <Route path='shop' element={<Shop/>}/>
-        <Route path='sign-in' element={<Authentication/>}/>
+        <Route path='auth' element={<Authentication/>}/>
       </Route>
     </Routes>
   );
--- src/routes/Navigation/navigation.component.jsx
+++ src/routes/Navigation/navigation.component.jsx
@@ -13,7 +13,7 @@ const Navigation = () => {
             </Link>
             <div className="nav-links-container">
                 <Link className="nav-link" to='/shop'>SHOP</Link>
-                <Link className="nav-link" to='/sign-in'>SIGN IN</Link>
+                <Link className="nav-link" to='/auth'>SIGN IN</Link>
             </div>
         </div>
         <Outlet/>
