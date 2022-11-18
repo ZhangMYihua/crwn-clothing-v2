6de9ea901db5fde86b5809d09d99b688f31e3ba4
Philip <philip.tan.venice@gmail.com>
2022-11-18 16:58:34 -0700

    testing commit

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,15 +1,20 @@
-import CategoryItem from '../category-item/category-item.component';
-
+import CategoryItem from "../category-item/category-item.component";
 import './directory.styles.scss';
 
-const Directory = ({ categories }) => {
-  return (
-    <div className='directory-container'>
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+const Directory = ({categories}) => {
+
+   return (
+      <div className='directory-container'>
+      {
+        categories.map((category) => (
+           <CategoryItem key={category.id} category={category} />
+        ))
+      }
+     </div>
+   );
+
 };
 
 export default Directory;
+
+
