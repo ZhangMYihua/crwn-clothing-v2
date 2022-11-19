9da231cefb5a530068476ebfd544b203c9b4ae2d
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2022-11-19 19:51:01 -0300

    crw-03 - displayin cart items total in the cart-icon.component / adding the total and setTotal state items into the cart.contexts

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,7 +7,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-  const { isOpen, setCartIsOpen } = useContext(CartContext);
+  const { isOpen, setCartIsOpen, total } = useContext(CartContext);
   const toogleCartOpen = () => {
     setCartIsOpen(!isOpen);
   };
@@ -15,7 +15,7 @@ const CartIcon = () => {
   return (
     <div className='cart-icon-container' onClick={toogleCartOpen}>
       <ShoppingIcon className='shopping-icon'/>
-      <span className='item-count'>0</span>
+      <span className='item-count'>{ total }</span>
     </div>
   )
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
@@ -15,18 +15,25 @@ export const CartContext = createContext({
   isOpen: false,
   setCartIsOpen: () => {},
   cartItems: [],
-  addItemToCart: () => {}
+  addItemToCart: () => {},
+  total: 0
 });
 
 export const CartProvider = ({ children }) => {
   const [isOpen, setCartIsOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isOpen, setCartIsOpen, addItemToCart, cartItems };
+  useEffect(() => {
+    const result = cartItems.reduce((acc, curr) => acc += curr.quantity, 0);
+    setTotal(result);
+  }, [cartItems]);
+
+  const value = { isOpen, setCartIsOpen, addItemToCart, cartItems, total };
 
   return <CartContext.Provider value={value}>{ children }</CartContext.Provider>;
 }
