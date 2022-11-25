47a1a337835cdd876f34d77c1c82a96eeeccd77d
ahmadDev <ahmadjafari6050@gmail.com>
2022-11-25 22:57:29 +0330

    payment with netlify and stripe done

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,11 +13,34 @@ const PaymentForm = () => {
             return;
         }
 
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({amount: 10500})
+        }).then( res => res.json());
+        const { paymentIntent: { client_secret }} = response;
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'ahmad j'
+                }
+            }
+        });
+        if (paymentResult.error) {
+            alert(paymentResult.error)
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('payment successful')
+            }
+        }
     }
     
     return ( 
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit card payment:</h2>
                 <CardElement></CardElement>
                 <Button buttonType={ BUTTON_TYPES_CLASSES.inverted}>Pay so fast</Button>
