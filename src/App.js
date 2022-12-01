46bab56879fc6d39328b2aec4f7be8dc25b99e68
Washington <washingtonjaaj@gmail.com>
2022-12-01 11:10:44 -0300

    modifing category

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 const App = () => {
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
+    <div className="categories-container">
+      <div className="category">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Hats</h2>
-          <p>Shop Now</p>
+          <p>Shop now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Jackets</h2>
-          <p>Shop Now</p>
+          <p>Shop now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Sneakers</h2>
-          <p>Shop Now</p>
+          <p>Shop now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Womens</h2>
-          <p>Shop Now</p>
+          <p>Shop now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Mens</h2>
-          <p>Shop Now</p>
+          <p>Shop now!</p>
         </div>
       </div>
     </div>
