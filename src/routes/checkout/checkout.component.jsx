b9776a3b1e6404bda7c74c08055564025cdef7be
Nathaniel <nielb4all@gmail.com>
2022-12-22 14:29:58 -0800

    code clean up with div and h2 removal in checkout.component 123

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -27,22 +27,20 @@ const Checkout = () => {
           <span>Remove</span>
         </div>
       </div>
-      <h1>I am the checkout page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-              <br />
-              <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
-              <br />
-              <span onClick={() => addItemToCart(cartItem)}>increment</span>
-            </div>
-          );
-        })}
-      </div>
+
+      {cartItems.map((cartItem) => {
+        const { id, name, quantity } = cartItem;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+            <br />
+            <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
+            <br />
+            <span onClick={() => addItemToCart(cartItem)}>increment</span>
+          </div>
+        );
+      })}
     </div>
   );
 };
