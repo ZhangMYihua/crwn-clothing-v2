5d7d23f55aefbe6c946763573542e40d8624058c
Nathaniel <nielb4all@gmail.com>
2022-12-22 17:54:59 -0800

    removed previous function and imported the CheckoutItem into the in checkout.component 123

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 
 import { CartContext } from "../../contexts/cart.context";
 
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import "./checkout.styles.scss";
 
 const Checkout = () => {
@@ -28,19 +30,10 @@ const Checkout = () => {
         </div>
       </div>
 
-      {cartItems.map((cartItem) => {
-        const { id, name, quantity } = cartItem;
-        return (
-          <div key={id}>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
-            <br />
-            <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
-            <br />
-            <span onClick={() => addItemToCart(cartItem)}>increment</span>
-          </div>
-        );
-      })}
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+
       <span className="Total">Total: 0</span>
     </div>
   );
