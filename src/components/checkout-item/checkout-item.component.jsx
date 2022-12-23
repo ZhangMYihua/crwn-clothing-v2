20f8ea873b000b8bad5a66e7a1f2dc9ac0ffdb1a
Nathaniel <nielb4all@gmail.com>
2022-12-22 17:39:35 -0800

    set up the component structure as well as the CheckoutItem container in checkout-item 123

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,18 @@
 import './checkout-item.scss'
 
-const CheckoutItem = () => {
-
+const CheckoutItem = (cartItem) => {
+const {name, imageUrl, price, quantity}
+return (
+    <div className='checkout-item-container'>
+        <div className='image-container'>
+            <img src={imageUrl} alt={`${name}`} />
+        </div>
+        <span></span>
+        <span></span>
+        <span></span>
+        <div className=''></div>
+    </div>
+)
 }
 
 
