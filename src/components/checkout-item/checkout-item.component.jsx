0790ab0ce6cc7b6428ad9456a9832e71469660df
Nathaniel <nielb4all@gmail.com>
2022-12-22 17:45:38 -0800

    set className for name, quantity, price, and a remove-button in checkout-item 123

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,16 @@
 import './checkout-item.scss'
 
 const CheckoutItem = (cartItem) => {
-const {name, imageUrl, price, quantity}
+const {name, imageUrl, price, quantity} = cartItem;
 return (
     <div className='checkout-item-container'>
         <div className='image-container'>
             <img src={imageUrl} alt={`${name}`} />
         </div>
-        <span></span>
-        <span></span>
-        <span></span>
-        <div className=''></div>
+        <span className='name'>{name}</span>
+        <span className='quantity'>{quantity}</span>
+        <span className='price'>{price}</span>
+        <div className='remove-button'>&#1005;</div>
     </div>
 )
 }
