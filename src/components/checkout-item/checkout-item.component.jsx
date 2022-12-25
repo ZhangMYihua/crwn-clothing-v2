0018cb7ae7efae78b0184b8a62a6428cee79896e
Nathaniel <nielb4all@gmail.com>
2022-12-25 14:44:45 -0800

    import useContext and CartContext into checkout-item.component  124

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import './checkout-item.scss'
 
 const CheckoutItem = ({cartItem}) => {
 const {name, imageUrl, price, quantity} = cartItem;
+
+
+
 return (
     <div className='checkout-item-container'>
         <div className='image-container'>
