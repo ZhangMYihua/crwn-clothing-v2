dc268a95271acf1758a8aed4c5ee437ccdb70a90
Nathaniel <nielb4all@gmail.com>
2022-12-25 14:47:42 -0800

    added clearItemFromCart and clearItemHandler into checkout-item.component  124

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,26 +1,27 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import { CartContext } from '../../contexts/cart.context';
+import { CartContext } from "../../contexts/cart.context";
 
-import './checkout-item.scss'
+import "./checkout-item.scss";
 
-const CheckoutItem = ({cartItem}) => {
-const {name, imageUrl, price, quantity} = cartItem;
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
 
+  const { clearItemFromCart } = useContext(CartContext);
 
+  const clearItemHandler = () => clearItemFromCart(cartItem);
 
-return (
-    <div className='checkout-item-container'>
-        <div className='image-container'>
-            <img src={imageUrl} alt={`${name}`} />
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>
+  return (
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={`${name}`} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={clearItemHandler}>&#10005;</div>
     </div>
-)
-}
-
+  );
+};
 
 export default CheckoutItem;
