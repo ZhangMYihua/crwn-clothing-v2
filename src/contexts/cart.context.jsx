dda0284fa9fce880713337d6229f525b56e3cf4b
Nathaniel <nielb4all@gmail.com>
2022-12-28 22:32:07 -0800

    added in new useEffect newCartTotal in cart.context  125

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -49,13 +49,23 @@ export const CartContext = createContext({
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
   cartCount: 0,
+  total: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+  
   useEffect(() => {
     const newCartCount = cartItems.reduce(
       (total, cartItem) => total + cartItem.quantity,
