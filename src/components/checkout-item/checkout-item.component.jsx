005e0cc6879b276d05d77fc22d68ebcc80b1ea75
Nathaniel <nielb4all@gmail.com>
2022-12-28 21:37:27 -0800

    added event functions into there respective onClick in checkout-item.component  124

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -10,6 +10,7 @@ const CheckoutItem = ({ cartItem }) => {
   const { clearItemFromCart, addItemToCart, removeItemToCart } =
     useContext(CartContext);
 
+    // Item handlers
   const clearItemHandler = () => clearItemFromCart(cartItem);
   const addItemHandler = () => addItemToCart(cartItem);
   const removeItemHandler = () => removeItemToCart(cartItem);
@@ -21,9 +22,9 @@ const CheckoutItem = ({ cartItem }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <div className="arrow">&#10094;</div>
+        <div className="arrow" onClick={addItemHandler}>&#10094;</div>
         <span className="value">{quantity}</span>
-        <div className="arrow">&#10095;</div>
+        <div className="arrow" onClick={removeItemHandler}>&#10095;</div>
       </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={clearItemHandler}>
