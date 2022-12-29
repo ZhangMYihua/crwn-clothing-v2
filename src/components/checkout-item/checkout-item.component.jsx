c865d52abcd136625b6ab57f7ac871abe3fa8e53
Nathaniel <nielb4all@gmail.com>
2022-12-28 21:40:53 -0800

    fix for the added event functions into there respective onClick in checkout-item.component  124

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -22,9 +22,9 @@ const CheckoutItem = ({ cartItem }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <div className="arrow" onClick={addItemHandler}>&#10094;</div>
+        <div className="arrow" onClick={removeItemHandler}>&#10094;</div>
         <span className="value">{quantity}</span>
-        <div className="arrow" onClick={removeItemHandler}>&#10095;</div>
+        <div className="arrow" onClick={addItemHandler}>&#10095;</div>
       </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={clearItemHandler}>
