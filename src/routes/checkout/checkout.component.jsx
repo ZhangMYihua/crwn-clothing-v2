32c0a387537f1689acb717f6aaf450566cbb2099
Nathaniel <nielb4all@gmail.com>
2022-12-28 22:38:25 -0800

    pulled new cartTotal into checkout.component  125

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -7,7 +7,7 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
     <div className="checkout-container">
@@ -32,7 +32,7 @@ const Checkout = () => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <span className="total">Total: 0</span>
+      <span className="total">Total: {cartTotal}</span>
     </div>
   );
 };
