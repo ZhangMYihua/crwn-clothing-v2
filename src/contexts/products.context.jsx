5b0b024030cd1c0e059332a2fb4ee3753925a882
Nathaniel <nielb4all@gmail.com>
2022-12-30 12:40:03 -0800

    update the products.context with SHOP_DATA changes  127

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,13 @@
 import { createContext, useState } from "react";
 
-import PRODUCTS from '../shop-data.json'
+import SHOP_DATA from '../shop-data.js'
 
 export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
     const value = {products};
     return (
         <ProductsContext.Provider value={value}> 
