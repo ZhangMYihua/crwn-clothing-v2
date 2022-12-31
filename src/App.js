4b784646d692b6dbf91de3cece26526a3c897d22
Mubi9000 <shabbirmuhammad2000@gmail.com>
2022-12-31 18:55:16 -0500

    sass syling added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss'
+
 const App = () => {
 
   const categories = [
@@ -26,7 +28,7 @@ const App = () => {
     <div className='categories-container'>
     {categories.map(({title}) => (
       <div className='category-container'>
-        {/* <img /> */}
+        <div className="background-image"/>
         <div className='category-body-container'>
           <h2>{title}</h2>
           <p>Shop Now</p>
