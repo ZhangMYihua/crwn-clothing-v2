26f3e7d8ba6b67636ae379c89075d2e9ad9ed4ae
Mubi9000 <shabbirmuhammad2000@gmail.com>
2022-12-31 20:46:43 -0500

    Lesson 3 files with failed attempt to create seperate files for the components code

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
 import './categories.styles.scss'
 
+import CategoryItem from './components/category-item/category-item.component'
+import CategoryMenu from './components/category-menu/category-menu.component.jsx'
+
 const App = () => {
 
-  const categories = [
+  // const categories = [
     
-      {
-        "id": 1,
-        "title": "hats",
-        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        "id": 2,
-        "title": "jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        "id": 3,
-        "title": "sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        "id": 4,
-        "title": "womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        "id": 5,
-        "title": "mens",
-        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-      }
-    ]
+  //     {
+  //       "id": 1,
+  //       "title": "hats",
+  //       "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  //     },
+  //     {
+  //       "id": 2,
+  //       "title": "jackets",
+  //       "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  //     },
+  //     {
+  //       "id": 3,
+  //       "title": "sneakers",
+  //       "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  //     },
+  //     {
+  //       "id": 4,
+  //       "title": "womens",
+  //       "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  //     },
+  //     {
+  //       "id": 5,
+  //       "title": "mens",
+  //       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  //     }
+  //   ]
   
-  return (
-    <div className='categories-container'>
-    {categories.map(({title, id, imageUrl}) => (
-      <div key={id}className='category-container'>
-        <div className="background-image" style= {{
-          backgroundImage:`url(${imageUrl})`
-        }}
-          
-        />
-        <div className='category-body-container'>
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    ))}
-    
-    </div>
-  )
+  // return (
+  //   <div className='categories-container'>
+  //     {categories.map((category) => (
+  //       <CategoryItem key={category.id} category={category}/>
+  //    ))}
+  //  </div>
+// )
 }
+
+
 export default App;
---
+++ src/components/category-menu/category-menu.component.jsx
@@ -0,0 +1,44 @@
+import CategoryItem from './components/category-item/category-item.component'
+import './category-menu.styles.scss'
+
+const menuItems = () => {
+
+const categories = [
+    
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+
+return (
+  <div className='categories-container'>
+    {categories.map((category) => (
+      <CategoryItem key={category.id} category={category}/>
+   ))}
+ </div>
+)
+}
+
+export default menuItems
