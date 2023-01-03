07593924c0ac23bbe655f9f109d7fd0a60ea1431
Nathaniel <nielb4all@gmail.com>
2023-01-02 21:26:23 -0800

    add useEffect for the addCollectionAndDocuments in products.context  128

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,20 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-import SHOP_DATA from '../shop-data.js'
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
-    products: [],
+  products: [],
 });
 
-export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState([]);
-    const value = {products};
-    return (
-        <ProductsContext.Provider value={value}> 
-            {children}
-        </ProductsContext.Provider>
-
-    )
-}
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    addCollectionAndDocuments('categories', SHOP_DATA)
+  }, []);
+  const value = { products };
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
