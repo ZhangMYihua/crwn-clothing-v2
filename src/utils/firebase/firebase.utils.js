e79875eb9ea7a822fa012ff972393e5b324909dc
Nathaniel <nielb4all@gmail.com>
2023-01-04 22:02:36 -0800

    added the querySnapshot with categoryMap in firebase.utils  129

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -67,6 +67,14 @@ export const addCollectionAndDocuments = async (
 
 export const getCategoriesAndDocuments = async () => {
   const collectionRef = collection(db,'categories')
+const q = query(collectionRef);
+
+const querySnapshot = await getDocs(q);
+const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+  const {title, items} = docSnapshot.data();
+  acc[title.toLowerCase()] = items;
+  return acc;
+}, {})
 }
 
 export const createUserDocumentFromAuth = async (
