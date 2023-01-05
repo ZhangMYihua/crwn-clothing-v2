0e18890bb31b317ba236ecee7dd2463cd9de2814
Nathaniel <nielb4all@gmail.com>
2023-01-04 23:07:52 -0800

    added Fragment in shop.component  131

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,23 +1,24 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
+  
   return (
-    <>
+    <Fragment>
       {Object.keys(categoriesMap).map((title) => (
-        <div key={title}>
+        <Fragment key={title}>
           <h2>{title}</h2>
           <div className="products-container">
             {categoriesMap[title].map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
           </div>
-        </div>
+        </Fragment>
       ))}
-    </>
+    </Fragment>
   );
 };
 
