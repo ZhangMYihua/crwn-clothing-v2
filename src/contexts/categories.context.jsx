5dff1c6d700eb9a4338d80a3b77e32024358972a
Nathaniel <nielb4all@gmail.com>
2023-01-04 22:44:55 -0800

    called the setCategoriesMap in categories.context  131

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -13,6 +13,7 @@ export const CategoriesProvider = ({ children }) => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     };
 
     getCategoriesMap();
