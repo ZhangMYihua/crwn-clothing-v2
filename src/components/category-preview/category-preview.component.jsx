75b80ea3f0fc9aca7817141b6c0d93943dd7f530
Nathaniel <nielb4all@gmail.com>
2023-01-08 19:12:53 -0800

    continued setup within category-preview-container in category-preview.component  132

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -3,7 +3,9 @@ import './category-preview.styles.scss'
 const CategoryPreview = ({title, products}) => {
 return (
     <div className='category-preview-container'>
-
+<h2>
+    <span>{title.toUpperCase}</span>
+</h2>
     </div>
 )
 }
