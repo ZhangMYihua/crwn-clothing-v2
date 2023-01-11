645ec8e40157e7f6226be90d38f801926fec1900
Nathaniel <nielb4all@gmail.com>
2023-01-10 22:10:02 -0800

    added the className to title and set product.filter for category-preview-container in category-preview.component  132

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -4,8 +4,13 @@ const CategoryPreview = ({title, products}) => {
 return (
     <div className='category-preview-container'>
 <h2>
-    <span>{title.toUpperCase}</span>
+    <span className='title'>{title.toUpperCase()}</span>
 </h2>
+<div className='preview'>
+    {
+        products.filter((_, idx) => idx < 4)
+    }
+</div>
     </div>
 )
 }
