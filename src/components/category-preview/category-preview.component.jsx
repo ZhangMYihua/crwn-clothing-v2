69e6e111cdbf478d7382a6a76cc92864f81aa204
Nathaniel <nielb4all@gmail.com>
2023-01-11 20:26:59 -0800

    continued setup for the products filter map for the preview in category-preview.component  132

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,5 @@
+import ProductCard from '../product-card/product-card.component';
+
 import './category-preview.styles.scss'
 
 const CategoryPreview = ({title, products}) => {
@@ -8,7 +10,10 @@ return (
 </h2>
 <div className='preview'>
     {
-        products.filter((_, idx) => idx < 4)
+        products
+        .filter((_, idx) => idx < 4)
+        .map((product) => 
+        <ProductCard key={product.id} product={product}/>)
     }
 </div>
     </div>
