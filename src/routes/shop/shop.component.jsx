53f36f94b7370695082bac3d76d27956f59b0b4a
Nathaniel <nielb4all@gmail.com>
2023-01-13 20:31:22 -0800

    import the CategoryPreview into shop.component  132

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,13 @@
 import { useContext, Fragment } from "react";
+
 import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
 import "./shop.styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
-  
+
   return (
     <Fragment>
       {Object.keys(categoriesMap).map((title) => (
