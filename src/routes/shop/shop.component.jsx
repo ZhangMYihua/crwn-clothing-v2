7c72632e3b6bb16053f659f5bd68902891920faf
Nathaniel <nielb4all@gmail.com>
2023-01-17 22:45:25 -0800

    restructured the shop-container object in  shop.component  132

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -10,17 +10,10 @@ const Shop = () => {
 
   return (
     <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      ))}
-    </Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        
+      })}
+    </div>
   );
 };
 
