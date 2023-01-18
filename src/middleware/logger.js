9888e47d4c185bb166acac0b9ea82f8bc7d14fed
Oscar Delgado <oscardel413@gmail.com>
2023-01-18 01:41:00 -0700

    clean up middleware logger

---
+++ src/middleware/logger.js
@@ -0,0 +1,15 @@
+
+//we can use this instead of reduc logger
+export const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('type', action.type);
+    console.log('payload', action.payload);
+    console.log('currentState', store.getState());
+
+    next(action);
+
+    console.log('next state', store.getState());
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,25 +1,12 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger'
+import logger from 'redux-logger'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from './root-reducer';
+import { loggerMiddleware } from '../middleware/logger';
 
 // root-reducer
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-
-    console.log('type', action.type);
-    console.log('payload', action.payload);
-    console.log('currentState', store.getState());
-
-    next(action);
-
-    console.log('next state', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -29,10 +16,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middlewares = [loggerMiddleware];
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
-const composedEnhancer = compose(applyMiddleware(...middlewares));
+const composedEnhancer = (process.env.NODE_ENV !== 'production' 
+&& window 
+&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) 
+|| compose;
 
-export const store = createStore(persistedReducer, undefined,composedEnhancer)
+const composedEnhancers = composedEnhancer(applyMiddleware(...middlewares));
+
+export const store = createStore(persistedReducer, undefined,composedEnhancers)
 
 export const persistor = persistStore(store)
