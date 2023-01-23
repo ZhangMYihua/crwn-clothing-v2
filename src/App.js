188c2d9d59f1ef1393b14c01771583963f1b7058
Daniel-Spicer <daniel__spicer@hotmail.com>
2023-01-24 08:50:22 +1100

    updating our directory page with directory componenet and category components~

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
   const categories = [
     {
-      id: 1,
-      title: 'Hats',
+      "id": 1,
+      "title": "Hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'Jackets',
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 3,
-      title: 'Sneakers',
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 4,
-      title: 'Womens',
+      "id": 4,
+      "title": "Womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 5,
-      title: 'Mens',
-    },
-  ];
+      "id": 5,
+      "title": "Mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+  
 
   return (
-    <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+   <Directory categories={categories}/>
   );
 };
 
