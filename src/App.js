a3ccde6557e9de94fcbd95cf9bfd4ef4ce8aa66d
Kalindu <kalindu.mk@gmail.com>
2023-01-24 10:31:27 +0530

    updating our directory page with directory componenet and category item

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import './categories.styles.css'
+
+import Directory from './components/directory/directory.component';
 
 const App = () => {
 
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ]
+  const categories = 
+    [
+      {
+        "id": 1,
+        "title": "hats",
+        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      },
+      {
+        "id": 2,
+        "title": "jackets",
+        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      },
+      {
+        "id": 3,
+        "title": "sneakers",
+        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      },
+      {
+        "id": 4,
+        "title": "womens",
+        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      },
+      {
+        "id": 5,
+        "title": "mens",
+        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+      },
+  ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({title, id}) => (
-        <div key={id} className="category-container">
-          <div className="background-image" /> 
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>     
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory categories={categories} />
   )
 };
 
