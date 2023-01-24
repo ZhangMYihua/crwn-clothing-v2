498f1dae9d41edbf348d991647f335456bfd18d2
Kalindu <kalindu.mk@gmail.com>
2023-01-24 15:01:10 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from "./routes/home/home.component";
 
-import Directory from './components/directory/directory.component';
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation abr</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return (
+    <h1>I am the shop page</h1>
+  )
+}
 
 const App = () => {
 
-  const categories = 
-    [
-      {
-        "id": 1,
-        "title": "hats",
-        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        "id": 2,
-        "title": "jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        "id": 3,
-        "title": "sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        "id": 4,
-        "title": "womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        "id": 5,
-        "title": "mens",
-        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-      },
-  ];
-
   return (
-    <Directory categories={categories} />
-  )
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
