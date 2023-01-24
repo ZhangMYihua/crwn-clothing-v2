f6c140db58c29c561f455121ec05f546936eac46
Nathaniel <nielb4all@gmail.com>
2023-01-23 22:14:42 -0800

    copied over code from shop.component and modified for categories-preview  134

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+import "./categories-preview.styles.scss";
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <div className="category-preview-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CategoriesPreview;
