42baedcfd4a2c57b41f128311912319049558b7e
Nathaniel <nielb4all@gmail.com>
2023-01-25 21:38:01 -0800

    added Category import with new route path in shop.component  135

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 import "./shop.styles.scss";
 
@@ -9,6 +10,7 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview /> } />
+      <Route path=":category" element={<Category /> } />
     </Routes>
   );
 };
