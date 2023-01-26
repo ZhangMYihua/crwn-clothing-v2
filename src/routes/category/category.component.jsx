3dcfcfb9e0eb8583fa421dbffc0ec43cafceeb04
Nathaniel <nielb4all@gmail.com>
2023-01-25 21:46:31 -0800

    set the useEffect with map through category in category.component  135

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -9,6 +9,11 @@ const Category = () => {
   const { category } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(categoriesMap[category]);
+    }, [category, categoriesMap])
+
 };
 
 export default Category;
