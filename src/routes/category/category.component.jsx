e8778ba1956b3e15c479e7e1bc3f62b47941de96
Nathaniel <nielb4all@gmail.com>
2023-01-25 21:50:45 -0800

    import the ProductCard and set a product return map with that ProductCard in category.component  135

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,8 @@
 import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import ProductCard from "../../components/product-card/product-card.component";
+
 import { CategoriesContext } from "../../contexts/categories.context";
 
 import "./category.styles.scss";
@@ -10,10 +12,17 @@ const Category = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        setProducts(categoriesMap[category]);
-    }, [category, categoriesMap])
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
+  return (
+    <div>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default Category;
