43f93c99ab70612febdddfe82c68d278393a510a
Nathaniel <nielb4all@gmail.com>
2023-01-25 21:43:18 -0800

    import useContext, useState, useEffect, useParams with calls in category.component  135

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,14 @@
-import './category.styles.scss'
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+
+import "./category.styles.scss";
 
 const Category = () => {
-
-}
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+};
 
 export default Category;
