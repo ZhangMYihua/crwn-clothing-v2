cb1e9042ffc45fe9e716218997bc0ebfc4e7b889
Nathaniel <nielb4all@gmail.com>
2023-01-25 21:24:31 -0800

    Code rework importing in the CategoriesPreview from component with a route in categories-preview  134

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, Fragments } from "react";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
@@ -7,14 +7,14 @@ const CategoriesPreview = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
+    <Fragments>
       {Object.keys(categoriesMap).map((title) => {
         const products = categoriesMap[title];
         return (
           <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-    </>
+    </Fragments>
   );
 };
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,15 @@
-import { useContext } from "react";
+import { Routes, Route } from 'react-router-dom'
 
-import { CategoriesContext } from "../../contexts/categories.context";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview /> } />
+    </Routes>
   );
 };
 
