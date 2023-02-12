6458a55bd4a7668102897688e6004bca42c2bffc
Dima <mthwhsn@gmail.com>
2023-02-12 18:28:34 +0200

    sign in component

---
+++ binco-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,58 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeEte4qTv9yPYU1sJ-nVp8R_xKVJburMk",
+
+  authDomain: "binco-clothing-db.firebaseapp.com",
+
+  projectId: "binco-clothing-db",
+
+  storageBucket: "binco-clothing-db.appspot.com",
+
+  messagingSenderId: "528787836271",
+
+  appId: "1:528787836271:web:834ee8130548663cfe2c9a",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user ", error.message);
+    }
+  }
+
+  return userDocRef;
+};
