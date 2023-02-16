a1a61649acae47a8bd2014828da03290baf9f5e8
Pavel Vslv <114096039+veselovp@users.noreply.github.com>
2023-02-16 23:28:29 +0000

    add categories

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import './Components/Categories-items/categories.styles.scss'
 
+import CategoryMenu from "./Components/Categories-items/CategoryMenu"
 
 const App = () => {
-  const categoriees = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ]
+  
   return (
-    <div className="categories-container">
-      {categoriees.map((item) => (
-        <div className="category-container">
-          <img src={item.imageUrl} alt="image" />
-          <div className="category-body-container">
-            <h2>{item.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <CategoryMenu />
   )
 }
 
