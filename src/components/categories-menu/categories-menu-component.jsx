a16a53440006513cb4a73df70a7a33e6e313c1cb
Anthony Diaz <anthonycrazycuba@gmail.com>
2023-03-01 02:56:20 -0500

    updating our directory page whit category-menu components & category-items components

---
+++ src/components/categories-menu/categories-menu-component.jsx
@@ -0,0 +1,22 @@
+import './categories-menu-styles.scss';
+import CategoryItem from '../category-items/category-item.components';
+
+const CategoryMenu = ({categories}) => {
+
+return (
+
+<div className="categories-container">
+   {categories.map( (category) => (
+
+    <CategoryItem id={category.id} category={category} />
+   )
+
+   )}
+    </div>
+);
+    
+
+}
+
+
+export default CategoryMenu
