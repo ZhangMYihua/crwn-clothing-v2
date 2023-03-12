c48173c9daa650347480073ef762698c513f260e
gabypega1997 <gabypega1997@gmail.com>
2023-03-12 18:18:25 +0100

    change a shop-data.ts

--- netlify/functions/create-payment-intent.ts
+++ netlify/functions/create-payment-intent.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+
 exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
