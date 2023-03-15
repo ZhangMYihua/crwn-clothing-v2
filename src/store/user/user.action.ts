2e9df88656945b240af6beea3088b05405f7e40e
gabypega1997 <gabypega1997@gmail.com>
2023-03-15 11:39:55 +0100

    finished my user store with typescript

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -1,24 +1,49 @@
 import { USER_ACTION_TYPES } from "./user.types";
 import {
     ActionWithPayload,
+    Action,
     createAction,
     withMatcher,
 } from "../../utils/reducer/reducer.utils";
 import { User } from "firebase/auth";
 
 // ********   Types **************
+// **** types for reducer****
+export type SignInSuccess = ActionWithPayload<
+    USER_ACTION_TYPES.SIGN_IN_SUCCESS,
+    User
+>;
+
+export type SignOutSuccess = Action<USER_ACTION_TYPES.SIGN_OUT_SUCCESS>;
+
+export type SignInFailed = ActionWithPayload<
+    USER_ACTION_TYPES.SIGN_IN_FAILED,
+    Error
+>;
+export type SignUpFailed = ActionWithPayload<
+    USER_ACTION_TYPES.SIGN_UP_FAILED,
+    Error
+>;
+export type SignOutFailed = ActionWithPayload<
+    USER_ACTION_TYPES.SIGN_OUT_FAILED,
+    Error
+>;
+
+// ----------- finished types for reducer -------
 
 export type SetCurrentUser = ActionWithPayload<
     USER_ACTION_TYPES.SET_CURRENT_USER,
     User
 >;
 
+export type CheckUserSession = Action<USER_ACTION_TYPES.CHECK_USER_SESSION>;
+
+export type GoogleSignInStart = Action<USER_ACTION_TYPES.GOOGLE_SIGN_IN_START>;
+
 // ********   Actions ********************
 
-export const setCurrentUser = withMatcher(
-    (user: User): SetCurrentUser =>
-        createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
-);
+export const setCurrentUser = (user: User) =>
+    createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 
 export const checkUserSession = () =>
     createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
@@ -29,11 +54,15 @@ export const googleSignInStart = () =>
 export const emailSignInStart = (email: string, password: string) =>
     createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email, password });
 
-export const signInSuccess = (user: User) =>
-    createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user);
+export const signInSuccess = withMatcher(
+    (user: User): SignInSuccess =>
+        createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)
+);
 
-export const signInFailed = (error: Error) =>
-    createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
+export const signInFailed = withMatcher(
+    (error: Error): SignInFailed =>
+        createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error)
+);
 
 export const signUpStart = (
     email: string,
@@ -52,14 +81,19 @@ export const signUpSuccess = (user: User, additionalDetails: string) =>
         additionalDetails,
     });
 
-export const signUpFailed = (error: Error) =>
-    createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
+export const signUpFailed = withMatcher(
+    (error: Error): SignUpFailed =>
+        createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error)
+);
 
 export const signOutStart = () =>
     createAction(USER_ACTION_TYPES.SIGN_OUT_START);
 
-export const signOutSuccess = () =>
-    createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS);
+export const signOutSuccess = withMatcher(
+    (): SignOutSuccess => createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS)
+);
 
-export const signOutFailed = (error: Error) =>
-    createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error);
+export const signOutFailed = withMatcher(
+    (error: Error): SignOutFailed =>
+        createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error)
+);
--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -1,6 +1,15 @@
 import { AnyAction } from "redux";
 import { USER_ACTION_TYPES } from "./user.types";
 
+import {
+    signInFailed,
+    signOutFailed,
+    signUpFailed,
+    signInSuccess,
+    signOutSuccess,
+} from "./user.action";
+import { act } from "@testing-library/react";
+
 const INITIAL_STATE = {
     currentUser: null,
     isLoading: false,
@@ -8,19 +17,36 @@ const INITIAL_STATE = {
 };
 
 export const userReducer = (state = INITIAL_STATE, action: AnyAction) => {
-    const { type, payload } = action;
-
-    switch (type) {
-        case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-            return { ...state, currentUser: payload };
-        case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
-            return { ...state, currentUser: null };
-
-        case USER_ACTION_TYPES.SIGN_OUT_FAILED:
-        case USER_ACTION_TYPES.SIGN_IN_FAILED:
-        case USER_ACTION_TYPES.SIGN_UP_FAILED:
-            return { ...state, error: payload };
-        default:
-            return state;
+    if (signInSuccess.match(action)) {
+        return { ...state, currentUser: action.payload };
     }
+    if (signOutSuccess.match(action)) {
+        return { ...state, currentUser: null };
+    }
+
+    if (signInFailed.match(action)) {
+        return { ...state, error: action.payload };
+    }
+    if (signOutFailed.match(action)) {
+        return { ...state, error: action.payload };
+    }
+
+    if (signUpFailed.match(action)) {
+        return { ...state, error: action.payload };
+    }
+
+    return state;
+
+    // switch (type) {
+    //     case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+    //         return { ...state, currentUser: payload };
+    //     case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
+
+    //     case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+    //     case USER_ACTION_TYPES.SIGN_IN_FAILED:
+    //     case USER_ACTION_TYPES.SIGN_UP_FAILED:
+    // return { ...state, error: payload };
+    //     default:
+    //         return state;
+    // }
 };
