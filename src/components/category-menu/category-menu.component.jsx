aefabc55cc579038002fdfedc00dabd4c5124e3b
Alexandru Moisescu <alexandrumoisescu@Alexandrus-MacBook-Pro.local>
2023-03-23 13:24:20 +0200

    feat: updated category components and category menu component

---
+++ src/components/category-menu/category-menu.component.jsx
@@ -0,0 +1,13 @@
+import './categories-menu.styles.scss'
+
+import {CategoryItem} from '../category-item/category-item.component'
+
+export const CategoryMenu = ({categories}) => {
+    return (
+        <div className='categories-container'>
+            {categories.map(({category}) => (
+                <CategoryItem key={category.id} category={category}></CategoryItem>
+            ))}
+        </div>
+    )
+}
