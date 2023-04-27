fa59d09389a58d07f610eec12e97cb43f94e16bf
ts <timors23@protonmail.com>
2023-04-27 21:52:27 +0300

    a

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
         {/* <img /> */}
         <div className='category-body-container'>
           <h2>Hats</h2>
-          <p>Shop Now</p>
+          <p>Shop Now zzzzzzz!!!</p>
         </div>
       </div>
       <div className='category-container'>
