b9beb622de49f9f26a8aec7df095badff986bef0
tguss76 <tguss76@gmail.com>
2023-07-17 16:01:21 +0200

    added chekout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,20 +1,28 @@
 import { ShoppingCartContext } from '../../context/shoppingCart.context';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
 
 
 import './cart-dropdown.styles.scss';
 import { CartContext } from '../../context/cart.context';
+ 
     
     const CartDropdown = () => {
-        const { cartItems } = useContext(CartContext);    
+        const { cartItems } = useContext(CartContext);
+        const navigate = useNavigate();
+        
+        const goToCheckoutHandler = () => {
+            navigate('/checkout')
+        }
+
         return (
             <div className='cart-dropdown-container'>
                 <div className='cart-items'>
                   {cartItems.map((item) => (<CartItem key={item.id} cartItem={item}/>))}  
                 </div>
-                <Button>GO TO CHECKOUT</Button>
+                <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
             </div>
             ) 
         }
---
+++ src/routes/checkout/checkout.componet.jsx
@@ -0,0 +1,11 @@
+import './checkout.styles.scss'
+
+const Checkout = () => {
+    return (
+     <diV>
+        <h1>I am the checkout page</h1>
+     </diV>   
+    ); 
+}
+
+export default Checkout;
