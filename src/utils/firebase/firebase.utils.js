34e683a702468120177cadffbf59c8582b3adeaa
Wilson Co <37131236+mi16co2012@users.noreply.github.com>
2023-07-17 23:13:07 +0800

    fixing code

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -33,5 +33,20 @@ const firebaseConfig = {
     console.log(userSnapShot);
     console.log(userSnapShot.exists());
 
+    if(!userSnapShot.exists()){
+      const [displayName, email] = userAuth;
+      const createdAt = new Date();
 
+    try {
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        createdAt
+      });
+    }catch (error){
+      console.log('error creating user', error.message);
+    }
+    return userDocRef;
+
+    }
   }
