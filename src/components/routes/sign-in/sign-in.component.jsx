67f959d36bb032b3aa079f9825d75d374bfe26b7
Wilson Co <37131236+mi16co2012@users.noreply.github.com>
2023-07-17 15:13:30 +0800

    added signin  in firebase
    this code is working

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,11 @@
-import { signInWithGooglePopup } from "../../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup, createUserDocumentFromAuth  } from "../../../utils/firebase/firebase.utils";
+
 
 const SignIn = () =>{
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response)
-    }
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    };
 
     return(
         <div>
