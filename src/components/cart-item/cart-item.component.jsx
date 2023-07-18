794e2dcef7643d17a777b9b6e14c13167493eb99
tguss76 <tguss76@gmail.com>
2023-07-18 14:08:46 +0200

    updated checkout page with total, increase decreas btn and remove from cart

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,17 @@
 import './cart-item.styles.scss'
 
 const CartItem = ({ cartItem }) => {
- const { name, imageUrl, price, quantity } = cartItem;
+const { name, imageUrl, price, quantity } = cartItem;
  return (
-    <div className='cart-item-container'>
-     <img src={imageUrl} alt={`${name}`} />
-     <div className='item-details'>
+  <div className='cart-item-container'>
+    <img src={imageUrl} alt={`${name}`} />
+    <div className='item-details'>
         <span className='name'>{name}</span>
         <span className='price'>{quantity} x ${price}
-        </span>
-      </div>
+      </span>
     </div>
+  </div>
  );
 }
 
-export default CartItem; 
+export default CartItem;
