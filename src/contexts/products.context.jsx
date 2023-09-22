701aadf977cf4734981b066e4abe672265bcd165
Serik <askaroff_serik@mail.ru>
2023-09-22 20:50:42 +0300

    fix: убрал не используевые переменые

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -3,11 +3,11 @@ import { createContext, useState } from 'react';
 import PRODUCTS from '../shop-data.json';
 
 export const ProductsContext = createContext({
-  products: [],
+  products: []
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products] = useState(PRODUCTS);
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
