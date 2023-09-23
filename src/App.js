c61e6d56e6dc9689a995701287c7ef873468a68d
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2023-09-23 17:22:09 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.component'
-import Navigation from './components/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
 
 export const Shop = () => {
   return (
@@ -17,9 +17,9 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
-        <Route index element={<Home />} /> 
-        <Route path='shop' element={<Shop />} /> 
-        <Route path='sign' element={<SignIn />} /> 
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
