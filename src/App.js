32ddf9fb151b959247a9358ab1d5c1aca05b5a01
yashviradia <y.viradia@tu-braunschweig.de>
2023-10-20 02:13:18 +0200

    corrected mistake in import to './routes/home/home.component'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.componentt';
+import Home from './routes/home/home.component';
 
 const App = () => {
   return (
