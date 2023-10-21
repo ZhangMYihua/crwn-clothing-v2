d64e72a92fbf552b362c8a2c368f6ec7efa37a21
yashviradia <y.viradia@tu-braunschweig.de>
2023-10-21 07:11:01 +0200

    corrected typo to 'Add to cart'

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -11,7 +11,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to card</Button>
+      <Button buttonType='inverted'>Add to cart</Button>
     </div>
   );
 };
