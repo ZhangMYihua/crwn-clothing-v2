165bde07c5e2fe9681f35504a881b1ca061ae646
Chandu_._._ <chandan03chandu@gmail.com>
2023-11-21 19:40:43 +0530

    categories updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
+import { Catagories } from "./component/Catagories";
 const App = () => {
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+    <div className='app'>
+      <Catagories/>
+      
     </div>
   );
 };
---
+++ src/component/Catagories.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import cat from "./../categories.json"
+import "./CatagoriesStyle.styles.scss"
+export const Catagories = () => {
+    console.log(cat);
+  return (
+    
+    <div className='categories-container'>
+   
+        {cat.map((d)=>(
+        <div className='category-container'>
+          <div className="img" style={{backgroundImage:`url(${d.imageUrl})`}} />
+          <div className='category-body-container'>
+            <h2>{d.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))
+        }
+    </div>
+  )
+}
