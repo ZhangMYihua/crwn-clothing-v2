2d78a7e9d0af034a23817c25ca681e1963330d8c
Ksenia <varosianarmen92@gmail.com>
2023-12-17 19:12:29 +0100

    made the pr.card

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './context/user.context';
+import { ProductsProvider } from './context/products.context';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,14 @@
-import SHOP_DATA from '../../shopdata.json';
+import { useContext } from 'react';
+import { ProductsContext } from '../../context/products.context';
+import ProductCard from '../../components/product-card/product-card.component';
+import './shop.styles.scss';
 
 const Shop = () => {
+    const { products } = useContext(ProductsContext);
     return (
-        <div>
-            
-            {SHOP_DATA.map(({ id, name }) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        <div className='products-container'>
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
             ))}
         </div>
     )
