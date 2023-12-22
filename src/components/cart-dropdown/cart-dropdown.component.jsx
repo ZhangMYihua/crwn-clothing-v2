0e3705fd6ec4cb5884390b66a9de983b6f89ac60
Ksenia <varosianarmen92@gmail.com>
2023-12-22 22:27:14 +0100

    start to build checkoutPage

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,18 @@
-import {React , useContext} from 'react';
+import { React, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './cart-dropdown.styles.scss';
 import { CartContext } from '../../context/cart.context';
 import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
 
-
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
 
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
 
     return (
         <div className='cart-dropdown-container'>
@@ -15,7 +20,7 @@ const CartDropdown = () => {
                 {cartItems.map((item) => (
                     <CartItem key={item.id} cartItem={item} />))}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </div>
     );
 };
