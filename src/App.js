da1b0ad80e5917897f6fa43b80afcb81affb950f
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-01-29 02:04:12 +0200

    79. Category Item Component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import "./categories.styles.scss";
+import CategoryItem from "./components/categoryItem/CategoryItem";
 
 const App = () => {
   const categories = [
     {
+      id: 1,
       title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
       id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      title: "jakets",
       id: 3,
-    },
-    {
       title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
       id: 4,
-    },
-    {
       title: "womens",
-      id: 5,
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
+      id: 5,
       title: "mens",
-      id: 6,
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
-        <div key={id} className="category-container">
-          {/* <img /> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
---
+++ src/components/categoryItem/CategoryItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./categoryItem.scss";
+export default function CategoryItem({ category }) {
+  const { title, id, imageUrl } = category;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+}
