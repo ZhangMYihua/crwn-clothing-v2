f4e09335fe4f93dde8ef21f946da8d951f4ecf0b
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-01-29 10:48:21 +0200

    85. React Router Outlet

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
