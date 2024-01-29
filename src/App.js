041a9015f536c194bbfc95f16e19b55a605287bb
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-01-29 13:05:22 +0200

    86. Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 // import Directory from "./components/directory/Directory";
 import Home from "./routes/home";
 
+const Navigation = () => {
+  return (
+    <>
+      <div>
+        <h1>This is navigation bar </h1>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+const Shop = () => {
+  return <h1>This is shop page </h1>;
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
