b3c310e3384aaac3e4dabb1a57ccef6dcd79041d
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-01-30 13:40:38 +0200

    97. Sign In With Redirect

--- src/routes/auth/login/Login.jsx
+++ src/routes/auth/login/Login.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 import {
+  // auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  // signInwithGoogleRedirect,
 } from "../../../utils/firbase/firebaseutils";
 function Login() {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await getRedirectResult(auth);
+  //       if (response) {
+  //         const userDocRef = await createUserDocumentFromAuth(response.user);
+  //         console.log(
+  //           "userCreated  after signInwithGoogleRedirect",
+  //           userDocRef
+  //         );
+  //       }
+  //     } catch (error) {
+  //       console.log("error from logincomponent", error);
+  //     }
+  //   };
+  //   fetchData();
+  //   // console.log("getUserRedirectResult", response);
+  // }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    console.log(user);
+    console.log("logGoogleUser", user);
     const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(userDocRef);
+    console.log("user created succesfuly in Firebase", userDocRef);
   };
 
-
-  
   return (
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>sign in with google pop up </button>
+      {/* <button onClick={signInwithGoogleRedirect}>
+        sign in with google redirect
+      </button> */}
     </div>
   );
 }
--- src/utils/firbase/firebaseutils.js
+++ src/utils/firbase/firebaseutils.js
@@ -21,14 +21,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInwithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
