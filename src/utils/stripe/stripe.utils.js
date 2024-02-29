337ed150f29e31705318f87338a68381e65027fe
Anthony Diaz <tdmedinaw@gmail.com>
2024-02-29 11:45:31 -0500

    fixing .env files

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-  "pk_test_51Omi8dKULFZGI9MBPWeCAbVlRoZuES9RdFyuLl4cW1zsKPJ5lWfSwC0TPY98WZDwmulRvCVKpoJMjJeYGgzLG2s500EmqGfkc7"
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
